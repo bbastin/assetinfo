625919795898ece9e1539047d9bef132661c376c
Benedikt Bastin <benedikt@benedikt-bastin.de>
2024-08-03 00:03:41 +0200

    test: Add tests for Version comparison

--- src/program.rs
+++ src/program.rs
@@ -65,3 +65,50 @@ impl PartialOrd for Version {
         self.patch.partial_cmp(&other.patch)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn version(major: usize, minor: Option<usize>, patch: Option<usize>) -> Version {
+        Version {
+            major,
+            minor,
+            patch,
+            string: String::default(),
+            cycle: String::default(),
+            extra: None,
+        }
+    }
+
+    #[test]
+    fn comparisons() {
+        let v111 = version(1, Some(1), Some(1));
+        let v112 = version(1, Some(1), Some(2));
+        let v121 = version(1, Some(2), Some(1));
+        let v122 = version(1, Some(2), Some(2));
+        assert!(v111 < v112);
+        assert!(v111 < v121);
+        assert!(v111 < v112);
+        assert!(v111 < v122);
+
+        assert!(v112 < v121);
+        assert!(v112 < v122);
+        assert!(v121 < v122);
+
+        assert_eq!(v111, v111);
+        assert_eq!(v112, v112);
+        assert_eq!(v121, v121);
+        assert_eq!(v122, v122);
+
+        assert_ne!(v111, v112);
+        assert_ne!(v111, v121);
+        assert_ne!(v111, v121);
+        assert_ne!(v111, v122);
+
+        assert!(v111 > version(1, None, None));
+        assert!(v111 > version(1, Some(1), None));
+        assert!(v111 < version(2, Some(1), None));
+        assert!(v111 < version(2, None, None));
+    }
+}
